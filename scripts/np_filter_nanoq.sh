0e796e61e6bb05f70a8166770c46acbddcc6df34
Kim <k.youngw@gmail.com>
2023-03-01 10:22:03 -0500

    Create read length filtering script for nanoq
    	- Add np_filter_nanoq.sh
    	- Create directory for singularity images
    	- Add nanoq.0.8.6.sif to singularity dir
    
    Will eventually remove singularity images and create an install.sh
    script to download necessary images. Singularity images were built
    using singularity/2.6.1 because scicomp modules are broken still.

---
+++ scripts/np_filter_nanoq.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+#This script will run nanoq to filter reads <1000bp using singularity
+
+#HELP function
+function HELP {
+echo ""
+echo "Usage:" $0
+echo "			-i path/to/reads			input reads in .fastq.gz format"
+echo "			-l minimum length			reads below this length will be filtered out"
+echo "			-o path/to/output			output filtered reads"
+echo ""
+echo "Example: $0 -i /path/to/reads -l 1000 -o path/to/output"
+echo ""
+exit 0
+}
+
+###Take arguments
+#Run HELP if -h -? or invalid input
+#Set READS to -i
+#Set MINLENGTH to -l
+#Set OUTPUT to -o
+while getopts ":hi:l:o:" option; do
+	case ${option} in
+		h)
+		HELP
+		;;
+		i)
+		export READS=${OPTARG}
+		;;
+		l)
+		export MINLENGTH=${OPTARG}
+		;;
+		o)
+		export OUTPUT=${OPTARG}
+		;;
+		\?)
+		echo "Invalid option: ${OPTARG}" 1>&2
+		HELP
+		;;
+	esac
+done
+
+#Check if all parameters are filled
+if [[ -z "${READS}" || -z "${MINLENGTH}" || -z "${OUTPUT}" ]]; then
+	echo ""
+	echo "All flags required."
+	HELP
+fi
+
+#Check if files exist and print out variables
+if [[ -e $READS ]]; then
+	echo ""
+	echo "$0"
+	echo "Input reads set to: " ${READS}
+	echo "Minimum length set to: " ${MINLENGTH}
+	echo "Output file set to: " ${OUTPUT}
+	echo ""
+else
+	echo ""
+	echo "Could not validate files. Please check and try again."
+	HELP
+fi
+
+thisDir=$(dirname $0)
+
+#Run nanoq - for now version is stuck at 0.8.6. 
+#TODO: Update with latest image if available.
+singularity exec -B $PWD:/data ${thisDir}/../singularity/nanoq.0.8.6.sif nanoq -i ${READS} -l ${MINLENGTH} -o ${OUTPUT} -vvv -H >> nanoq.log 2>&1
