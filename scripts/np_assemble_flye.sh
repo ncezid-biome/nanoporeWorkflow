82d42ffa8712564cdc3f26a54dff54731cf68d88
Kim <k.youngw@gmail.com>
2023-03-07 15:29:02 -0500

    Modify flye script: 	* Modify np_assembly_flye.sh assembly script 	* Add args for read path, genome size, and output directory 	* Add singularity commands to run flye off of staphb docker image

--- scripts/np_assemble_flye.sh
+++ scripts/np_assemble_flye.sh
@@ -1,84 +1,70 @@
 #!/bin/bash
-#$ -o flye.log
-#$ -e flye.err
-#$ -j y
-#$ -N flye
-#$ -pe smp 2-16
-#$ -l h_vmem=64G
-#$ -V -cwd
-set -e
 
-# This script will take the output of np_filter_filtlong.sh and assemble using flye.
+#This script will take the output from np_downsample_rasusa.sh and assembly using flye
 
-### REQUIREMENTS:
-# singularity
-# access to flye singularity image at /apps/standalone/singularity/flye/flye.2.7.staphb.simg
-
-### USAGE:
-# NOTE: barcode-dir/ MUST end with a forward slash '/'
-#
-# /path/to/nanoporeWorkflow/scripts/np_assemble_flye.sh barcode-dir/
-
-#This function will check to make sure the directory doesn't already exist before trying to create it
-make_directory() {
-    if [ -e $1 ]; then
-        echo "Directory "$1" already exists"
-    else
-        mkdir $1
-        echo "Directory "$1" has been created"
-    fi
+#HELP function
+function HELP {
+echo ""
+echo "Usage:" $0
+echo "			-r /path/to/reads 			path to filtered, downsampled reads"
+echo "			-g genome 					size of genome e.g. 4.8MB"
+echo "			-o /path/to/output/			output directory"
+echo ""
+echo "Example: $0 -r /path/to/reads -g 4.8m -o path/to/output/directory/"
+echo ""
+exit 0
 }
 
-NSLOTS=${NSLOTS:=16}
-echo '$NSLOTS set to:' $NSLOTS
+###Take arguments
+#Run HELP if -h -? or invalid input
+#Set GENOMESIZE to -g
+#Set COVERAGE to -c
+#Set READS to -i
+#Set OUTPUT to -o
+while getopts ":hr:g:o:" option; do
+	case ${option} in
+		h)
+		HELP
+		;;
+		r)
+		export READS=${OPTARG}
+		;;
+		g)
+		export GENOMESIZE=${OPTARG}
+		;;
+		o)
+		export OUTPUT=${OPTARG}
+		;;
+		\?)
+		echo "Invalid option: ${OPTARG}" 1>&2
+		HELP
+		;;
+	esac
+done
 
-INDIR=$1
-echo '$INDIR is set to:' $INDIR
-
-GENOMELENGTH=5000000 # TODO make this a parameter
-LONGREADCOVERAGE=120  # How much coverage to target with long reads
-
-set -u
-
-if [ "$INDIR" == "" ]; then
-    echo "Usage: $0 barcode-fastq-dir/"
-    echo ""
-    exit 1;
-fi;
-
-# Setup any debugging information
-date
-hostname
-
-# Setup tempdir
-tmpdir=$(mktemp -p . -d flye.XXXXXX)
-trap ' { echo "END - $(date)"; rm -rf $tmpdir; } ' EXIT
-make_directory $tmpdir/log
-echo "$0: temp dir is $tmpdir";
-
-dir=$INDIR
-
-BARCODE=$(basename ${dir})
-echo '$BARCODE is set to:' $BARCODE
-
-# check to see if sample has already been assembled, skip if so
-if [[ -e ${dir}flye/assembly.fasta ]]; then
-  echo "Reads have been assembled by np_assemble_flye script already. Skipping...."
-  exit 0
+#Check if all parameters are filled
+if [[ -z "${READS}" || -z "${GENOMESIZE}" || -z "${OUTPUT}" ]]; then
+	echo ""
+	echo "All flags required."
+	HELP
 fi
 
-LENGTHS=${dir}readlengths.txt.gz
-echo '$LENGTHS set to:' $LENGTHS
+#Check if files exist and print out variables
+if [[ -e $READS ]]; then
+	echo ""
+	echo "$0"
+	echo "Genome size set to:" ${GENOMESIZE}
+	echo "Output directory set to: " ${OUTPUT}
+	echo ""
+else
+	echo ""
+	echo "Could not validate files. Please check and try again."
+	HELP
+fi
 
-# Determine minimum read length for desired coverage.
-# Do this by reading lengths from biggest to smallest,
-# stopping when we get to the desired coverage and saving
-# that read length.
-MINLENGTH=$(zcat "$LENGTHS" | sort -rn | perl -lane 'chomp; $minlength=$_; $cum+=$minlength; $cov=$cum/'$GENOMELENGTH'; last if($cov > '$LONGREADCOVERAGE'); END{print $minlength;}')
-echo "Min length for $LONGREADCOVERAGE coverage will be $MINLENGTH";
-
-# Assemble.
-echo "Assembling with flye..."
-singularity exec --no-home -B ${dir}:/data /apps/standalone/singularity/flye/flye.2.7.staphb.simg \
-  flye --nano-raw /data/reads.minlen500.600Mb.fastq.gz -o /data/flye -g 5m --plasmids -t $NSLOTS
-
+#Run flye version 2.9, latest available from staphb dockerhub repo as of 3/7/2023
+#NOTE: Params set to --nano-hq which means you need ONT high-quality reads: Guppy5+ SUP or Q20 (<5% error)
+#Change to --nano-raw for anything pre-Guppy4, --nano-corr for corrected reads
+#For more information on flye usage: https://github.com/fenderglass/Flye/blob/flye/docs/USAGE.md
+singularity exec -B $PWD:/data docker://staphb/flye:2.9 flye --version
+singularity exec -B $PWD:/data docker://staphb/flye:2.9 flye --nano-hq $READS -g $GENOMESIZE -o $OUTPUT
