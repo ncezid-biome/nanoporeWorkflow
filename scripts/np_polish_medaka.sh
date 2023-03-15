4050703d4ae4abc9aa7c93945a73f5e5b38d6f0b
Kim <k.youngw@gmail.com>
2023-03-15 12:48:14 -0400

    Modify medaka 	* Make changes to np_polish_medaka.sh 	* Run via Singularity container built from docker://ontresearch/medaka:latest

--- scripts/np_polish_medaka.sh
+++ scripts/np_polish_medaka.sh
@@ -1,54 +1,52 @@
 #!/bin/bash
-#$ -o medaka.log
-#$ -j y
-#$ -N medaka
-#$ -pe smp 2
-#$ -V -cwd
-set -e
 
-# OK, guess that there is no benefit beyond 2 threads, according to medaka 1.2.0 warning messages
-NSLOTS=${NSLOTS:=2}
-echo '$NSLOTS set to:' $NSLOTS
+#This program will polish the racon draft assembly using medaka via Singularity container
 
-INDIR=$1
+#HELP function
+function HELP {
+echo ""
+echo "Usage:" $0
+echo "	  -i path/to/reads/				path to reads"
+echo "          -d path/to/draft/assembly			path to draft racon assembly"
+echo "          -o path/to/output/dir/			output directory"
+echo "          -m model					medaka model to match guppy basecaller"
+echo ""
+echo "Example: $0 -i /path/to/longreads.fastq -d /path/to/raconassembly.fasta -o path/to/output/dir -m r1041_e82_260bps_sup_g632"
+echo ""
+exit 0
+}
 
-set -u
+###Take arguments
+#Run HELP if -h -? or invalid input
+#Set ASSEMBLY to -a
+#Set LONGREADS to -r
+while getopts ":hi:d:o:m:" option; do
+	case ${option} in
+		h)
+		HELP
+		;;
+		i)
+		export LONGREADS=${OPTARG}
+		;;
+		d)
+		export ASSEMBLY=${OPTARG}
+		;;
+		o)
+		export OUTPUT=${OPTARG}
+		;;
+		m)
+		export MODEL=${OPTARG}
+		;;
+		\?)
+		echo "Invalid option: ${OPTARG}" 1>&2
+		HELP
+		;;
+	esac
+done
 
-if [ "$INDIR" == "" ]; then
-    echo "Usage: $0 barcode-dir/"
-    exit 1;
-fi;
-
-# Setup any debugging information
-date
-hostname
-
-# Setup tempdir
-tmpdir=$(mktemp -p . -d medaka.XXXXXX)
-trap ' { echo "END - $(date)"; rm -rf $tmpdir; } ' EXIT
-echo "$0: temp dir is $tmpdir";
-
-dir=$INDIR
-echo '$dir is set to:' ${dir}
-BARCODE=$(basename ${dir})
-echo '$BARCODE is set to:' $BARCODE
-
-# commented out because unused variable
-#FASTQ="$dir/all.fastq.gz"
-
-# check to see if assembly has been polished, skip if so
-if [[ -e ${dir}medaka/consensus.fasta ]]; then
-  echo "Racon-polished assembly has already been polished with Medaka. Exiting...."
-  exit 0
-fi
-
-# print medaka version; run medaka 
-singularity exec --no-home -B $PWD:/data /apps/standalone/singularity/medaka/medaka.1.2.0.staphb.sif medaka --version
+# print medaka version; run medaka from ontresearch/medaka dockerhub repo: version 1.7.2 is latest as of 3/15/2023
+singularity exec -B $PWD:/data docker://ontresearch/medaka:latest medaka --version
 
 echo "Running Medaka via Singularity container..."
-singularity exec --no-home -B ${dir}:/data /apps/standalone/singularity/medaka/medaka.1.2.0.staphb.sif \
-medaka_consensus -i /data/reads.minlen500.600Mb.fastq.gz -m r941_min_high_g360 -t ${NSLOTS} -o /data/medaka -d /data/racon/ctg.consensus.iteration4.fasta
+singularity exec -B $PWD:/data docker://ontresearch/medaka:latest medaka_consensus -i $LONGREADS -d $ASSEMBLY -o $OUTPUT -m $MODEL
 
-# copy final medaka-polished asm up one to main barcode directory, name it something obvious
-# would be nice to later incorporate sample ID and run ID here later
-cp -nv ${dir}medaka/consensus.fasta ${dir}final.asm.${BARCODE}.fasta
